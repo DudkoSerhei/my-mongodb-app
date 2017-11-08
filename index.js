de65d44371542aa1b02bea62777b234292a6a5d8
DudkoSerhei <serhei.dudko@gmail.com>
2017-11-08 20:01:34 +0300

    Add movie functional

--- index.js
+++ index.js
@@ -1,16 +1,51 @@
 var express = require('express');
-var index = require('./routes/index.js');
+var bodyParser = require('body-parser');
+var MongoClient = require('mongodb').MongoClient;
+var assert = require('assert');
 
 var app = express();
-
 app.set('view engine', 'ejs');
-
 app.use(express.static(__dirname + '/public'));
+app.use(bodyParser.urlencoded({ extended: true }));
 
-app.get('/', index.index);
+function errorHandler(err, req, res, next) {
+    console.error(err.message);
+    console.error(err.stack);
+    res.status(500).render('error', { error: err });
+}
 
-var port = process.env.PORT || 3000;
+MongoClient.connect('mongodb://localhost:27017/video', function(err, db) {
 
-app.listen(port, function () {
-  console.log('Server is Up!');
+  assert.equal(null, err);
+  app.get('/', function(req, res, next) {
+        res.render('index', {});
+    });
+
+  app.post('/movie', function(req, res, next) {
+
+    var title = req.body.title;
+    var year = req.body.year;
+    var imdb = req.body.imdb;
+
+    if ((title == '') || (year == '') || (imdb == '')) {
+      next('Error, check the data!');
+    } else {
+        db.collection('movies').insertOne(
+          { 'title': title, 'year': year, 'imdb': imdb },
+          function (err, resp) {
+              assert.equal(null, err);
+              res.send("Paste _id: " + resp.insertedId);
+          }
+      );
+    }
+  });
+
+  app.use(errorHandler);
+
+  var port = process.env.PORT || 3000;
+
+  app.listen(port, function () {
+    console.log('Server is Up!');
+  });
+
 });
